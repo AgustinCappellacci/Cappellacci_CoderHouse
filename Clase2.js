954962a099eafa7e7b180d31d8f6da7b7f9543e9
AgustinCappellacci <86740433+AgustinCappellacci@users.noreply.github.com>
2022-03-05 10:12:21 -0300

    Update Clase2.js

--- Clase2.js
+++ Clase2.js
@@ -1,34 +1,68 @@
 const fs = require('fs');
 
-class Contenedor {
+module.exports = class Contenedor {
     constructor (archivo) {
         this.archivo = archivo;
         this.maxID = 0;
     }
 
     save(producto){
-        let p = {Titulo: producto.titulo, Precio: producto.precio, ID: this.maxID};
-        this.maxID++;
-        fs.appendFileSync(this.archivo,JSON.stringify(p));
-    }
+
+            let p = {Titulo: producto.titulo, Precio: producto.precio, Thumbnail: producto.thumbnail, ID: this.maxID};
+            this.maxID++;
+            fs.appendFileSync(this.archivo, `${JSON.stringify(p)}`)
+        }
+        
+        /*try{
+            let contenido = fs.readFileSync(this.archivo, "utf-8")
+            console.log(contenido)
+            let productos = JSON.parse(contenido)
+            productos.push(p)
+            fs.appendFileSync(this.archivo,JSON.stringify(productos))
+         } catch(err){
+            fs.appendFileSync(this.archivo,JSON.stringify(p))
+        }*/
+    
+
 
     getAll(){
-        let productos = []
-        productos.push(fs.readFileSync(this.archivo, 'utf-8'))
-        console.log(productos)
+        //let productos = []
+        //productos.push({nombre: "Agustin", apellido: "Cappellacci"})
+        //fs.writeFileSync(this.archivo, JSON.stringify(productos))
+        const contenido = fs.readFileSync(this.archivo, "utf-8")
+        const resultado = JSON.parse(contenido.split("\r\n"))
+        console.log(resultado)
     }
 
     deleteAll(){
         fs.unlinkSync(this.archivo);
+        console.log("Se eliminó archivo")
     }
 
-    //getByID(id)
+    getByID(id){
+        let productos = []
+        const contenido = fs.readFileSync(this.archivo, "utf-8")
+        productos = JSON.parse(contenido)
+        for (let i = 0; i <= productos.length; i++) {
+            if(productos[i].ID == id){
+                console.log(i)
+                return productos[i]
+            }
+        }
+        console.log("no se encontró el id indicado")
+    }
 
-    //deleteById(id)
+    deleteById(id){
+        let productos = []
+        const contenido = fs.readFileSync(this.archivo, "utf-8")
+        productos = JSON.parse(contenido)
+        for (let i = 0; i <= productos.length; i++) {
+            if(productos[i].ID == id){
+                productos.splice(id,1)
+                fs.writeFileSync(this.archivo,JSON.stringify(productos))
+                console.log("Se eliminó producto eitosamente")
+            }
+        }
+        console.log("no se encontró el id indicado")
+    }
 }
-
-const P = new Contenedor('productos.txt');
-P.save({titulo: "Televisor", precio: "USD 450"});
-P.save({titulo: "Telefono", precio: "USD 600"});
-P.save({titulo: "PS5", precio: "USD 499"});
-P.getAll();
